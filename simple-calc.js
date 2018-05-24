f41eef9dcb17fb16b4212546f4946bf87d980f36
Natasha Cox <natashac1014@gmail.com>
2018-05-24 09:43:18 -0500

    made the buttons work

--- simple-calc.js
+++ simple-calc.js
@@ -2,9 +2,24 @@ function multiplyBy () {
     let num1 = document.getElementById("fnumber").value;
     let num2 = document.getElementById("snumber").value;
     document.getElementById("result").innerHTML = num1 * num2;
-
-    console.log(num1);
-    console.log(num2);
+}
+function divideBy () {
+    let num1 = document.getElementById('fnumber').value;
+    let num2 = document.getElementById('snumber').value;
+    document.getElementById('result').innerHTML = num1 / num2;
+}
+function addTo () {
+    let num1 = document.getElementById('fnumber').value;
+    let num2 = document.getElementById('snumber').value;
+    document.getElementById('result').innerHTML = +num1 + +num2;
+}
+function subtractFrom () {
+    let num1 = document.getElementById('fnumber').value;
+    let num2 = document.getElementById('snumber').value;
+    document.getElementById('result').innerHTML = num1 - num2;
 }
 document.getElementById("mult").addEventListener("click", multiplyBy)
-console.log(document.getElementById("mult"))
+document.getElementById('divide').addEventListener('click', divideBy)
+document.getElementById('add').addEventListener('click', addTo)
+document.getElementById('sub').addEventListener('click', subtractFrom)
+
